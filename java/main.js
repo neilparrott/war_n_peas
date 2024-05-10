bea2f63f9c55ba515cb95cc819564794f5e1127f
Neil <77660430+neilparrott@users.noreply.github.com>
2024-05-10 15:32:59 +0100

    Update main.js
    moved countries array out into its own js file

--- java/main.js
+++ java/main.js
@@ -1,23 +1,5 @@
 function main() {
 
-const countries = [
-    "Afghanistan","Algeria","Argentina","Australia","Bangladesh",
-    "Barbados","Bolivia","Brazil","Canada","Chile",
-    "China","Colombia","Cuba","Denmark","Ecuador","Estonia",
-    "Ethiopia","Fiji","Finland","France","Germany","Greece","India","Indonesia",
-    "Iran","Iraq","Ireland","Italy","Japan","Kenya","Mexico","Monaco","Myanmar",
-    "Nigeria","Norway","Pakistan","Papua-New-Guinea","Paraguay","Peru",
-    "Philippines","Poland","Russia","Singapore","South-Africa","South-Korea",
-    "Spain","Sudan","Sweden","Tanzania","Thailand","Torres-Strait-Islanders",
-    "Uganda","UK","Vietnam","Israel","Palestine","Lebanon","Turkmenistan",
-    "Azerbaijan","Ukraine","Georgia","Uruguay","Venezuela","Panama","Nicaragua",
-    "Saudi-Arabia","Yemen","Honduras","Guyana","Suriname","Guatemala","Lesotho","Laos",
-    "Cambodia","Brunei","North-Korea","Belarus","Latvia","Lithuania","Bulgaria",
-    "Albania","Croatia","Serbia","Montenegro","Slovakia","Slovenia","Moldova",
-    "Czech-Republic","Nepal","Vanuatu","Namibia","Angola","Botswana","Mozambique",
-    "Zimbabwe","Zambia","Malawi","Congo-Democratic-Republic","Gabon","Cameroon",
-    "Chad","Ghana","Benin","Mali","Mauritania","Portugal"
-]; 
 var score = 0;
 function getUniqueRandomNumbers(min, max, count) {
   if (max - min + 1 < count) {
